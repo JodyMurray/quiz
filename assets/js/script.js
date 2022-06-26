df9ac0ff0cd5959f7c0c344c3591540459aefc05
Jody M <jodyamm@hotmail.com>
2022-06-26 16:19:27 +0000

    Add event listener and selected function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 
 
-
 //questions and answers
-var questions = [
+var questionData = [
     {
         question: "Who is Bilbo Baggins?",
-        answerA: "The elf",
-        answerB: "The dwarf",
-        answerC: "The hobbit",
+        a: "The elf",
+        b: "The dwarf",
+        c: "The hobbit",
         correctAnswer: "C"
     },   
     {
@@ -15,86 +14,135 @@ var questions = [
         answerA: "The group was trapped in a cave",
         answerB: "The leader of the group got lost",
         answerC: "The group was attacked by orcs",
-        correctAnswer: "C"
+        correctAnswer: "c"
     },
     {
         question: "What is the name of the wizard who always helps Frodo?",
-        answerA:"Gandalf",
-        answerB: "Saruman",
-        answerC: "Sauron",
-        correctAnswer: "A"
+        a:"Gandalf",
+        b: "Saruman",
+        c: "Sauron",
+        correctAnswer: "a"
     },
     {
         question: "What was the gift Galadriel gave to Frodo?",
-        answerA: "A sword",
-        answerB: "A starlight",
-        answerC: "A strand of her hair",
-        correctAnswer: "B"
+        a: "A sword",
+        b: "A starlight",
+        c: "A strand of her hair",
+        correctAnswer: "c"
 
     },
     {
         question: "Who was the heir to the throne in Minas Tirith?",
-        answerA: "Sauron",
-        answerB: "Legolas",
-        answerC: "Aragorn",
-        correctAnswer: "C"
+        a: "Sauron",
+        b: "Legolas",
+        c: "Aragorn",
+        correctAnswer: "c"
     },
     {
         question: "Who wanted to take over the kingdom of Rohan?",
-        answerA:"Gandalf",
-        answerB: "Sauron",
-        answerC: "Saruman",
-        correctAnswer: "C"
+        a:"Gandalf",
+        b: "Sauron",
+        c: "Saruman",
+        correctAnswer: "c"
     },
     {
         question: "Who has been following Sam and Frodo on their way from Moria to Mordor?",
-        answerA: "Gollum",
-        answerB: "Gimli",
-        answerC: "Legolas",
-        correctAnswer: "A"
+        a: "Gollum",
+        b: "Gimli",
+        c: "Legolas",
+        correctAnswer: "a"
     },
     {
         question: "What is the title of the first book of the series?",
-        answerA: "The Two Towers",
-        answerB: "The Return of the King",
-        answerC: "The Fellowship of the Ring",
-        correctAnswer: "C"
+        a: "The Two Towers",
+        b: "The Return of the King",
+        c: "The Fellowship of the Ring",
+        correctAnswer: "c"
     },  
     {
         question: "What are exactly the Two Watchers?",
-        answerA: "Carved statues on the entrance to the Cirith Ungol Tower",
-        answerB: "Massive statues of Anarion and Isildur standing on the River Anduin",
-        answerC: "Two rivers flowing through and creating the borders in the Gap of Rohan",
-        correctAnswer: "A"
+        a: "Carved statues on the entrance to the Cirith Ungol Tower",
+        b: "Massive statues of Anarion and Isildur standing on the River Anduin",
+        c: "Two rivers flowing through and creating the borders in the Gap of Rohan",
+        correctAnswer: "a"
     },
     {
         question: "What are Palantiri?",
-        answerA: "3 powerful given to the Elves.",
-        answerB: "7 seeing stones",
-        answerC: "3 jewels made by Fëanor",
-        correctAnswer: "B"
+        a: "3 powerful given to the Elves.",
+        b: "7 seeing stones",
+        c: "3 jewels made by Fëanor",
+        correctAnswer: "b"
     },
     {
         question: "Which of those isn't a public inn in the Middle Earth?",
-        answerA: "The Prancing Pony",
-        answerB: "The Green Dragon",
-        answerC: "The Southern Star",
-        correctAnswer: "C"
+        a: "The Prancing Pony",
+        b: "The Green Dragon",
+        c: "The Southern Star",
+        correctAnswer: "c"
     },
     {
         question: "Who is not affected by the Ring?",
-        answerA:"Galadriel",
-        answerB: "Bilbo",
-        answerC: "Sam",
-        correctAnswer: "C"
+        a:"Galadriel",
+        b: "Bilbo",
+        c: "Sam",
+        correctAnswer: "c"
     },
     {
         question: "What is Bilbo's home called?",
-        answerA: "Bag End",
-        answerB: "Bag Shot",
-        answerC: "Overhill",
-        correctAnswer: "A"
+        a: "Bag End",
+        b: "Bag Shot",
+        c: "Overhill",
+        correctAnswer: "a"
     },
-]
+];
 
+const quiz = document.getElementById('quiz');
+const answers = document.querySelectorAll('.answer');
+const questionP = document.getElementById('question');
+const aText = document.getElementById('a_text');
+const bText = document.getElementById('b_text');
+const cText = document.getElementById('c_text');
+const submitButton = document.getElementById('submit');
 
+let currentQuiz = 0;
+let score = 0;
+ 
+startQuiz()
+
+function startQuiz(){
+    deselectAns()
+    const currentQuizData = questionData[currentQuiz]
+
+    questionP.innerText = currentQuizData.question;
+    aText.innerText = currentQuizData.a;
+    bText.innerText = currentQuizData.b;
+    cText.innerText = currentQuizData.c;
+}
+function deselectAns() {
+    answers.forEach(answers=> answers.checked =false)
+}
+function getSelected(){
+    let answers;
+    answers.forEach(answers => {
+        if(answers.checked){
+            answer = answers.id;
+        }
+    });
+    return answer
+}
+submitButton.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === questionData[currentQuiz].correctAnswer){
+            score++;
+        }
+        currentQuiz++;
+
+        if(currentQuiz < questionData.length) {
+            startQuiz();
+        } else {
+            quiz.innerHTML =`<h2>You answeered ${score}/${questionData.length} questions correctly</h2>
+            <button onclick="location.reload()">Reload</button>`
+        }
+    }
+})
