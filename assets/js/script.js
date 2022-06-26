92cfe267dfcd1c9ae3513d1063630fa16c30bc32
Jody M <jodyamm@hotmail.com>
2022-06-26 15:48:35 +0000

    Change structure of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,182 +1,100 @@
-var quizSection = document.getElementById('quiz');
-var resultsSection = document.getElementById('results');
-var submitButton = document.getElementById('submit');
 
-function buildQuiz(questions, quizSection, resultsSection, submitButton){
 
-    function showQuestions(questions, quizSection){
-        var output = [];
-        var answers;
 
-        for(var i=0; i<questions.length; i++){
-            answers=[];
-
-            for(letter in questions[i].answers){
-                answers.push(
-                    '<label>'
-                    +'<input type="radio" name="question'+i+'"value="'+letter+'">'
-                    + letter+' '
-                    + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            output.push(
-                '<div class="question"> ' + questions[i].question + '</div>'
-                +'<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-            quizSection.innerHTML = output.join('');
-    }
-showQuestions(questions, quizSection);
-
-    function showResults(questions, quizSection, resultsSection){
-        var answerSections = quizSection.querySelectorAll('.answers');
-    
-    var userAns ='';
-    var numCorrect = 0;
-
-    for(var i=0; i<questions.length; i++){
-        userAns = (answerSections[i].queryselector('input[name=question'+i+']:checked') ||{}).value
-    
-    //correct answer
-    if(userAns===answerSections[i].correctAnswer){
-        numCorrect++;
-        
-        answerSections[i].style.color = 'lightblue';
-    } else {
-        answerSections[i].style.color = 'pink';
-    }
-    }
-    resultsSection.innerHTML =numCorrect + 'out of'+ questions.length;
-    }
-
-    showQuestions(questions, quizSection);
-
-    submitButton.onclick = function(){
-        showResults(questions, quizSection, resultsSection);
-    }
-}
 //questions and answers
-var allQuestions = [
+var questions = [
     {
         question: "Who is Bilbo Baggins?",
-        answers: {
-            a: "The elf",
-            b: "The dwarf",
-            c: "The hobbit",
-    },
-    correctAnswer: "c"
-},
+        answerA: "The elf",
+        answerB: "The dwarf",
+        answerC: "The hobbit",
+        correctAnswer: "C"
+    },   
     {
         question: "What happened to the Fellowship of the Ring soon after they've left Lothlórien?",
-        answers: {
-            a: "The group was trapped in a cave",
-            b: "The leader of the group got lost",
-            c: "The group was attacked by orcs",
-    },
-    correctAnswer: "c"
+        answerA: "The group was trapped in a cave",
+        answerB: "The leader of the group got lost",
+        answerC: "The group was attacked by orcs",
+        correctAnswer: "C"
     },
     {
         question: "What is the name of the wizard who always helps Frodo?",
-        answers: {
-            a:"Gandalf",
-            b: "Saruman",
-            c: "Sauron",
+        answerA:"Gandalf",
+        answerB: "Saruman",
+        answerC: "Sauron",
+        correctAnswer: "A"
     },
-    correctAnswer: "a"
-},
     {
         question: "What was the gift Galadriel gave to Frodo?",
-        answers: {
-            a: "A sword",
-            b: "A starlight",
-            c: "A strand of her hair",
+        answerA: "A sword",
+        answerB: "A starlight",
+        answerC: "A strand of her hair",
+        correctAnswer: "B"
+
     },
-    correctAnswer: "b"
-},
     {
         question: "Who was the heir to the throne in Minas Tirith?",
-        answers: {
-            a:"Sauron",
-            b: "Legolas",
-            c: "Aragorn",
+        answerA: "Sauron",
+        answerB: "Legolas",
+        answerC: "Aragorn",
         correctAnswer: "C"
     },
-    correctAnswer: "c"
-},
     {
         question: "Who wanted to take over the kingdom of Rohan?",
-        answers: {
-            a:"Gandalf",
-            b: "Sauron",
-            c: "Saruman",
+        answerA:"Gandalf",
+        answerB: "Sauron",
+        answerC: "Saruman",
+        correctAnswer: "C"
     },
-    correctAnswer: "c"
-},
     {
         question: "Who has been following Sam and Frodo on their way from Moria to Mordor?",
-            answers: {
-            a: "Gollum",
-            b: "Gimli",
-            c: "Legolas",
+        answerA: "Gollum",
+        answerB: "Gimli",
+        answerC: "Legolas",
+        correctAnswer: "A"
     },
-    correctAnswer: "a"
-},
     {
         question: "What is the title of the first book of the series?",
-        answers: {
-            a: "The Two Towers",
-            b: "The Return of the King",
-            c: "The Fellowship of the Ring",
-    },
-    correctAnswer: "c"
-    },
+        answerA: "The Two Towers",
+        answerB: "The Return of the King",
+        answerC: "The Fellowship of the Ring",
+        correctAnswer: "C"
+    },  
     {
         question: "What are exactly the Two Watchers?",
-        answers: {
-            a: "Carved statues on the entrance to the Cirith Ungol Tower",
-            b: "Massive statues of Anarion and Isildur standing on the River Anduin",
-            c: "Two rivers flowing through and creating the borders in the Gap of Rohan",
-    },
-    correctAnswer: "a"
+        answerA: "Carved statues on the entrance to the Cirith Ungol Tower",
+        answerB: "Massive statues of Anarion and Isildur standing on the River Anduin",
+        answerC: "Two rivers flowing through and creating the borders in the Gap of Rohan",
+        correctAnswer: "A"
     },
     {
         question: "What are Palantiri?",
-        answers: {
-            a: "3 powerful given to the Elves.",
-            b: "7 seeing stones",
-            c: "3 jewels made by Fëanor",
-    },
-    correctAnswer: "b"
+        answerA: "3 powerful given to the Elves.",
+        answerB: "7 seeing stones",
+        answerC: "3 jewels made by Fëanor",
+        correctAnswer: "B"
     },
     {
         question: "Which of those isn't a public inn in the Middle Earth?",
-        answers: {
-            a: "The Prancing Pony",
-            b: "The Green Dragon",
-            c: "The Southern Star",
-    },
-    correctAnswer: "c"
+        answerA: "The Prancing Pony",
+        answerB: "The Green Dragon",
+        answerC: "The Southern Star",
+        correctAnswer: "C"
     },
     {
         question: "Who is not affected by the Ring?",
-        answers: {
-            a:"Galadriel",
-            b: "Bilbo",
-            c: "Sam",
-    },
-    correctAnswer: "c"
+        answerA:"Galadriel",
+        answerB: "Bilbo",
+        answerC: "Sam",
+        correctAnswer: "C"
     },
     {
         question: "What is Bilbo's home called?",
-        answers: {
-            a: "Bag End",
-            b: "Bag Shot",
-            c: "Overhill",
+        answerA: "Bag End",
+        answerB: "Bag Shot",
+        answerC: "Overhill",
+        correctAnswer: "A"
     },
-    correctAnswer: "a"
-},
 ]
 
-buildQuiz(allQuestions, quizSection, resultsSection, submitButton);
+
