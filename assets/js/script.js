bb4a4dd29944e5eb62889edb67553cd85bf7ad92
Jody M <jodyamm@hotmail.com>
2022-06-26 06:39:00 +0000

    Add home image and opacity

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+var questions = [
+    {
+        question: "Who is Bilbo Baggins?",
+        answerA: "The elf",
+        answerB: "The dwarf",
+        answerC: "The hobbit",
+        correctAnswer: "C"
+    },
+    {
+        question: "What happened to the Fellowship of the Ring soon after they've left Lothlórien?",
+        answerA: "The group was trapped in a cave",
+        answerB: "The leader of the group got lost",
+        answerC: "The group was attacked by orcs",
+        correctAnswer: "C"
+    },
+    {
+        question: "What is the name of the wizard who always helps Frodo?",
+        answerA: "Gandalf",
+        answerB: "Saruman",
+        answerC: "Sauron",
+        correctAnswer: "A"
+    },
+    {
+        question: "What was the gift Galadriel gave to Frodo?",
+        answerA: "A sword",
+        answerB: "A starlight",
+        answerC: "A strand of her hair",
+        correctAnswer: "B"
+    },
+    {
+        question: "Who was the heir to the throne in Minas Tirith?",
+        answerA: "Sauron",
+        answerB: "Legolas",
+        answerC: "Aragorn",
+        correctAnswer: "C"
+    },
+    {
+        question: "Who wanted to take over the kingdom of Rohan?",
+        answerA: "Gandalf",
+        answerB: "Sauron",
+        answerC: "Saruman",
+        correctAnswer: "C"
+    },
+    {
+        question: "Who has been following Sam and Frodo on their way from Moria to Mordor?",
+        answerA: "Gollum",
+        answerB: "Gimli",
+        answerC: "Legolas",
+        correctAnswer: "A"
+    },
+    {
+        question: "What is the title of the first book of the series?",
+        answerA: "The Two Towers",
+        answerB: "The Return of the King",
+        answerC: "The Fellowship of the Ring",
+        correctAnswer: "C"
+    },
+    {
+        question: "What are exactly the Two Watchers?",
+        answerA: "Carved statues on the entrance to the Cirith Ungol Tower",
+        answerB: "Massive statues of Anarion and Isildur standing on the River Anduin",
+        answerC: "Two rivers flowing through and creating the borders in the Gap of Rohan",
+        correctAnswer: "A"
+    },
+    {
+        question: "What are Palantiri?",
+        answerA: "3 powerful given to the Elves.",
+        answerB: "7 seeing stones",
+        answerC: "3 jewels made by Fëanor",
+        correctAnswer: "B"
+    },
+    {
+        question: "Which of those isn't a public inn in the Middle Earth?",
+        answerA: "The Prancing Pony",
+        answerB: "The Green Dragon",
+        answerC: "The Southern Star",
+        correctAnswer: "C"
+    },
+    {
+        question: "Who is not affected by the Ring?",
+        answerA: "Galadriel",
+        answerB: "Bilbo",
+        answerC: "Sam",
+        correctAnswer: "C"
+    },
+    {
+        question: "What is Bilbo's home called?",
+        answerA: "Bag End",
+        answerB: "Bag Shot",
+        answerC: "Overhill",
+        correctAnswer: "A"
+    },
+]
