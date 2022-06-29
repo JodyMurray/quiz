f229bccfb9f502f5afae7a386401b8667803ff39
Jody M <jodyamm@hotmail.com>
2022-06-29 14:19:47 +0000

    Add background slide function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,18 +131,23 @@ function startQuiz(){
     bText.innerText = currentQuizData.b;
     cText.innerText = currentQuizData.c;
 }
-function deselectAns() {
+function deselectAns() 
+{
     answers.forEach(answers=> answers.checked =false)
 }
-function getSelected() {
+
+function getSelected() 
+{
     let answer
     answers.forEach(answers => {
         if (answers.checked) {
             answer = answers.id;
         }
     });
+
     return answer;
 }
+
 submitButton.addEventListener('click', () => {
     const answer = getSelected()
         if(answer === questionData[currentQuiz].correctAnswer){
@@ -157,10 +162,31 @@ submitButton.addEventListener('click', () => {
         }
     } 
 );
-// function getValue() {
-//     out1.innerHTML = text1.value;
-// }
+var i = 0;
+var images = [];
+var slideTime = 3000; // 3 seconds
 
-// // button.addEventListener('click', getValue);
-// var player = document.getElementById("player-name").value;
-//         document.getElementById("output").innerHTML = player;
+images[0] = 'https://i.guim.co.uk/img/media/fd1398dcde9ef858d15a5cbacd2d4d13fae94ee2/447_0_1986_1192/master/1986.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=a40ac39a948faf6f15c44d69fdb0e850';
+images[1] = 'https://irs.www.warnerbros.com/hero-banner-v2-tablet-jpeg/movies/media/browser/the_lord_of_the_rings_the_fellowship_of_the_ring_banner.jpeg';
+images[2] = 'https://static.dw.com/image/15912725_101.jpg';
+images[3] = 'https://assets.reedpopcdn.com/-1629897048023.jpg/BROK/thumbnail/1600x900/format/jpg/quality/80/-1629897048023.jpg';
+images[4] = 'https://ychef.files.bbci.co.uk/976x549/p04mbz2p.jpg';
+images[5] = 'https://www.dsogaming.com/wp-content/uploads/2020/12/The-Lord-of-the-Rings-The-Shire-Unreal-Engine-4.jpg';
+images[6] = 'https://c4.wallpaperflare.com/wallpaper/793/812/996/the-lord-of-the-rings-bag-end-the-shire-interiors-house-wallpaper-thumb.jpg';
+images[7] = 'https://4.bp.blogspot.com/-0_0N_ImKgNk/UDFHRCscN8I/AAAAAAAAAH4/ubjGvvV2Zv4/w1200-h630-p-k-no-nu/middle-earth-map.jpg';
+
+
+
+
+function changePicture() {
+    document.body.style.backgroundImage = "url(" + images[i] + ")";
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(changePicture, slideTime);
+}
+
+window.onload = changePicture;
